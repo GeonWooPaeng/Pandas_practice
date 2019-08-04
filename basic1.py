47c6105dacdc25b9c13c2d03daf58fa15ea33832
GeonWooPaeng <gwpaeng@naver.com>
2019-08-04 22:26:50 +0900

    basic 5

--- basic1.py
+++ basic1.py
@@ -190,3 +190,243 @@ df['introduce'] = df['age'].apply(get_introduce,prefix="I am",suffix=" years old
 df
 
 #%%
+def get_introduce2(row):
+    return "I was born in " + str(row.year) + "my age is " + str(row.age)
+df.introduce = df.apply(get_introduce2,axis=1)
+df
+
+#%%
+def extract_year(row):
+    return row.split('-')[0]
+
+date_list = [{'yyyy-mm-dd':'2000-06-27'},{'yyyy-mm-dd':'2002-09-24'},{'yyyy-mm-dd':'2005-12-20'}]
+df = pd.DataFrame(date_list, columns =['yyyy-mm-dd'])
+df
+
+#%%
+df['year'] = df['yyyy-mm-dd'].map(extract_year)
+df
+
+#%%
+job_list = [{'age':20,'job':'student'},{'age':30,'job':'developer'},{'age':30,'job':'teacher'}]
+df = pd.DataFrame(job_list)
+df
+
+#%%
+df.job = df.job.map({"student":1,"developer":2,"teacher":3})
+df
+#%%
+x_y = [{'x':5.5, 'y':-5.6},{'x':-5.2,'y':5.5},{'x':-1.6,'y':-4.5}]
+df = pd.DataFrame(x_y)
+df
+
+#%%
+df = df.applymap(np.around)
+df
+
+#%%
+friend_dict_list =[{'name':'John','midterm':95,'final':85},{'name':'Jenny','midterm':85,'final':80},{'name':'Nate','midterm':10,'final':30}]
+df = pd.DataFrame(friend_dict_list,columns = ['name','midterm','final'])
+df
+
+#%%
+df2 = pd.DataFrame([['Ben',50,50]],columns =['name','midterm','final'])
+df2.head()
+
+#%%
+df.append(df2, ignore_index=True)
+
+#%%
+student_list =[{'name':'John','major':"Computer Science", 'sex':"male"},{'name':'Nate','major':"Computer Science",'sex':"male"},{'name':'Abraham','major':"Physics",'sex':"male"},{'name':'Brian','major':"Psychology",'sex':"male"},{'name':'Janny','major':"Economics",'sex':"female"},{'name':'Yuna','major':"Economics",'sex':"female"},{'name':'Jeniffer','major':"Computer Science",'sex':"female"},{'name':'Edward','major':"Computer Science",'sex':"male"},{'name':'Zara','major':"Psychology",'sex':"female"},{'name':'Wendy','major':"Economics",'sex':"female"},{'name':'Sera','major':"Psychology",'sex':"female"}]
+df = pd.DataFrame(student_list,columns = ['name','major','sex'])
+df
+
+#%%
+groupby_major = df.groupby('major')
+
+#%%
+groupby_major.groups
+
+#%%
+for name, group in groupby_major:
+    print(name+": "+str(len(group)))
+    print(group)
+    print()
+
+#%%
+df_major_cnt = pd.DataFrame({'count':groupby_major.size()}).reset_index()
+df_major_cnt
+
+#%%
+groupby_sex = df.groupby('sex')
+
+#%%
+for name, group in groupby_sex:
+    print(name + ": " + str(len(group)))
+    print(group)
+    print()
+
+#%%
+df_sex_cnt = pd.DataFrame({'count' : groupby_sex.size()}).reset_index()
+df_sex_cnt
+
+#%%
+student_list = [{'name': 'John', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Nate', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Abraham', 'major': "Physics", 'sex': "male"},
+                {'name': 'Brian', 'major': "Psychology", 'sex': "male"},
+                {'name': 'Janny', 'major': "Economics", 'sex': "female"},
+                {'name': 'Yuna', 'major': "Economics", 'sex': "female"},
+                {'name': 'Jeniffer', 'major': "Computer Science", 'sex': "female"},
+                {'name': 'Edward', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Zara', 'major': "Psychology", 'sex': "female"},
+                {'name': 'Wendy', 'major': "Economics", 'sex': "female"},
+                {'name': 'Sera', 'major': "Psychology", 'sex': "female"},
+                {'name': 'John', 'major': "Computer Science", 'sex': "male"},
+         ]
+df = pd.DataFrame(student_list, columns = ['name', 'major', 'sex'])
+df
+
+#%%
+df.duplicated()
+
+#%%
+df = df.drop_duplicates()
+df
+
+#%%
+student_list = [{'name': 'John', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Nate', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Abraham', 'major': "Physics", 'sex': "male"},
+                {'name': 'Brian', 'major': "Psychology", 'sex': "male"},
+                {'name': 'Janny', 'major': "Economics", 'sex': "female"},
+                {'name': 'Yuna', 'major': "Economics", 'sex': "female"},
+                {'name': 'Jeniffer', 'major': "Computer Science", 'sex': "female"},
+                {'name': 'Edward', 'major': "Computer Science", 'sex': "male"},
+                {'name': 'Zara', 'major': "Psychology", 'sex': "female"},
+                {'name': 'Wendy', 'major': "Economics", 'sex': "female"},
+                {'name': 'Nate', 'major': None, 'sex': "male"},
+                {'name': 'John', 'major': "Computer Science", 'sex': None},
+         ]
+df = pd.DataFrame(student_list, columns = ['name', 'major', 'sex'])
+df
+
+#%%
+df.duplicated(['name'])
+
+#%%
+df.drop_duplicates(['name'],keep='last')
+
+#%%
+school_id_list = [{'name': 'John', 'job': "teacher", 'age': 40},
+                {'name': 'Nate', 'job': "teacher", 'age': 35},
+                {'name': 'Yuna', 'job': "teacher", 'age': 37},
+                {'name': 'Abraham', 'job': "student", 'age': 10},
+                {'name': 'Brian', 'job': "student", 'age': 12},
+                {'name': 'Janny', 'job': "student", 'age': 11},
+                {'name': 'Nate', 'job': "teacher", 'age': None},
+                {'name': 'John', 'job': "student", 'age': None}
+         ]
+df = pd.DataFrame(school_id_list, columns = ['name', 'job', 'age'])
+df
+
+#%%
+df.info()
+
+#%%
+df.isna()
+
+#%%
+df.isnull()
+
+#%%
+tmp = df
+tmp["age"] = tmp["age"].fillna(0)
+tmp
+
+#%%
+df["age"].fillna(df.groupby("job")["age"].transform("median"),inplace=True)
+
+#%%
+df
+
+#%%
+job_list = [{'name': 'John', 'job': "teacher"},
+                {'name': 'Nate', 'job': "teacher"},
+                {'name': 'Fred', 'job': "teacher"},
+                {'name': 'Abraham', 'job': "student"},
+                {'name': 'Brian', 'job': "student"},
+                {'name': 'Janny', 'job': "developer"},
+                {'name': 'Nate', 'job': "teacher"},
+                {'name': 'Obrian', 'job': "dentist"},
+                {'name': 'Yuna', 'job': "teacher"},
+                {'name': 'Rob', 'job': "lawyer"},
+                {'name': 'Brian', 'job': "student"},
+                {'name': 'Matt', 'job': "student"},
+                {'name': 'Wendy', 'job': "banker"},
+                {'name': 'Edward', 'job': "teacher"},
+                {'name': 'Ian', 'job': "teacher"},
+                {'name': 'Chris', 'job': "banker"},
+                {'name': 'Philip', 'job': "lawyer"},
+                {'name': 'Janny', 'job': "basketball player"},
+                {'name': 'Gwen', 'job': "teacher"},
+                {'name': 'Jessy', 'job': "student"}
+         ]
+df = pd.DataFrame(job_list, columns = ['name', 'job'])
+
+#%%
+print(df.job.unique())
+
+#%%
+df.job.value_counts()
+
+#%%
+l1 = [{'name': 'John', 'job': "teacher"},
+      {'name': 'Nate', 'job': "student"},
+      {'name': 'Fred', 'job': "developer"}]
+
+l2 = [{'name': 'Ed', 'job': "dentist"},
+      {'name': 'Jack', 'job': "farmer"},
+      {'name': 'Ted', 'job': "designer"}]
+         
+df1 = pd.DataFrame(l1, columns = ['name', 'job'])
+df2 = pd.DataFrame(l2, columns = ['name', 'job'])
+
+#%%
+frames = [df1,df2]
+result = pd.concat(frames,ignore_index=True)
+result
+
+#%%
+l1 = [{'name':'John','job':"teacher"},{'name':'Nate','job':"student"},{'name':'Fred','job':"developer"}]
+
+l2 = [{'name':'Ed','job':"dentist"},{'name':'Jack','job':"farmer"},{'name':'Ted','job':"designer"}]
+df1 = pd.DataFrame(l1, columns =['name','job'])
+df2 = pd.DataFrame(l2, columns =['name','job'])
+result = df1.append(df2,ignore_index=True)
+
+#%%
+result
+
+#%%
+l1 = [{'name': 'John', 'job': "teacher"},
+      {'name': 'Nate', 'job': "student"},
+      {'name': 'Jack', 'job': "developer"}]
+
+l2 = [{'age': 25, 'country': "U.S"},
+      {'age': 30, 'country': "U.K"},
+      {'age': 45, 'country': "Korea"}]
+
+df1 = pd.DataFrame(l1,columns = ['name','job'])
+df2 = pd.DataFrame(l2,columns = ['age','country'])
+result = pd.concat([df1,df2],axis=1,ignore_index=True)
+result
+
+#%%
+label = [1,2,3,4,5]
+prediction = [1,2,2,5,5]
+
+comparison = pd.DataFrame({'label':label,'prediction':prediction})
+comparison 
+
+#%%
